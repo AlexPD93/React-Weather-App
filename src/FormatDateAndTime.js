616a6694d187a118c775bca098293eac7875b83e
AlexPD93 <78558945+AlexPD93@users.noreply.github.com>
2021-12-08 14:21:16 -0600

    started to fix the date and time

--- src/FormatDateAndTime.js
+++ src/FormatDateAndTime.js
@@ -1,33 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function FormatDate(props) {
-  let now = new Date();
-  let hours = now.getHours();
-  if (hours < 10) hours = "0" + now.getHours();
+  const [exactTime, setexactTime] = useState({ ready: false });
 
-  let minutes = now.getMinutes();
-  if (minutes < 10) minutes = "0" + now.getMinutes();
-
-  let date = now.getDate();
-  if (date < 10) date = "0" + now.getDate();
-
-  let months = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-  ];
-  let month = months[now.getMonth()];
-
-  let year = now.getFullYear();
   function day() {
     let date = new Date(props.time * 1000);
     let day = date.getDay();
@@ -35,17 +11,32 @@ export default function FormatDate(props) {
     return days[day];
   }
 
-  return (
-    <div className="FormatDate">
-      <h3>
-        <span>{day()} </span>
-        <span>
-          {hours}:{minutes}
-        </span>
-      </h3>
-      <h3>
-        {date}/{month}/{year}
-      </h3>
-    </div>
-  );
+  function showLiveTime(response) {
+    setexactTime({
+      ready: true,
+      currentTime: response.data.datetime.slice(10, 16),
+      todayDate: response.data.datetime.slice(8, 10),
+      month: response.data.datetime.slice(5, 7),
+      year: response.data.datetime.slice(0, 4),
+    });
+  }
+
+  if (exactTime.ready) {
+    return (
+      <div className="FormatDate">
+        <h3>
+          <span>{day()} </span>
+          <span>{exactTime.currentTime}</span>
+        </h3>
+        <h3>
+          {exactTime.todayDate}/{exactTime.month}/{exactTime.year}
+        </h3>
+      </div>
+    );
+  } else {
+    let apiKey = `dac344694bff4911a980eb0b3d3c6c11`;
+    let apiUrl = `https://timezone.abstractapi.com/v1/current_time?api_key=${apiKey}&location=${props.city}`;
+    axios.get(`${apiUrl}`).then(showLiveTime);
+    return null;
+  }
 }
